9ac2e8a965be6fa4f52529fd713234b0207968c5
Sen Zhang <szhang.hust@gmail.com>
2024-07-03 20:44:12 +0800

    init

---
+++ helloworld/helloworld/helloworldApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloworldApp.swift
+//  helloworld
+//
+//  Created by Sen on 2024/7/3.
+//
+
+import SwiftUI
+
+@main
+struct helloworldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
